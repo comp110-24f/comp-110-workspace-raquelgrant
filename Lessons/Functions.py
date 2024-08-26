91061ae99fce60cf960498e9350379241662bb8e
Raquel Grant <raqueleg@unc.edu>
2024-08-26 15:19:53 -0400

    Finished Exercise 00!

---
+++ Lessons/Functions.py
@@ -0,0 +1,11 @@
+""""Practice with functions."""
+
+
+# Function Definition
+def sum(num1: int, num2: int) -> int:
+    """Print sum of num1 and num2"""
+    return num1 + num2
+
+
+# Function Call
+print(sum(num1=9, num2=10))
---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+""""My first exercise in COMP110!!"""
+
+__author__ = "730663103"
+
+
+def greet(name: str) -> str:
+    """ "A welcoming first function definition."""
+    return "Hello," + name + "!"
