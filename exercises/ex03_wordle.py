e0004a336a4164a00aeb2a4a0e98a9cac8d7d17f
Raquel Grant <raqueleg@unc.edu>
2024-10-02 02:51:34 -0400

    progress on EX 03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,72 +1,104 @@
 """"EX03 - Coding my own wordle!"""  # docstring explaining code
 
-__author__ = "730663103"
+__author__ = "730663103"  # who i am
 
 
 def input_guess(secret_word_len: int) -> str:  # defining fn. to get players guess word
-    while True:
-        guess = input(f"Enter a {secret_word_len}-character word: ")
+    while True:  # creating loop to make sure guess is length of secret word
+        guess = input(
+            f"Enter a {secret_word_len}-character word: "
+        )  # creating var. for input guess
         word_len: int = len(guess)
-        if word_len == secret_word_len:
-            return guess
+        if word_len == secret_word_len:  # if length of guess is length of secret word
+            return guess  # send guess to next part
         else:
-            print(f"That wasn't {secret_word_len} chars! Try again: ")
+            print(
+                f"That wasn't {secret_word_len} chars! Try again: "
+            )  # if wrong length loop back to original input question
 
 
-def contains_char(secret_word: str, char_guess: str) -> bool:
+def contains_char(
+    secret_word: str, char_guess: str
+) -> bool:  # defining fn. to check if specific character is in guessed word
     """Function that will look through secret word for specific characters."""
-    assert len(char_guess) == 1
-    letter_counter: int = 0
+    assert len(char_guess) == 1  # ensure charact is only 1 letter
+    letter_counter: int = 0  # setting index counter to zero
 
-    while letter_counter < len(secret_word):
-        if secret_word[letter_counter] == char_guess:
+    while letter_counter < len(
+        secret_word
+    ):  # making loop for while index counter is less than length of secret word
+        if (
+            secret_word[letter_counter] == char_guess
+        ):  # if letter at index counter matches specific char
             return True
-        letter_counter += 1
+        letter_counter += 1  # add to index counter to move to next letter in next loop
 
-    return False
+    return False  # if letter not found after looping through whole word
 
 
-def emojified(guess: str, secret: str) -> str:
+def emojified(
+    guess: str, secret: str
+) -> str:  # defining fn. to turn guesses into emojis
     """Function to change guessed letters into diff emojis based on accuracy"""
-    assert len(guess) == len(secret)
+    assert len(guess) == len(
+        secret
+    )  # ensure length of guess and length of secret word are the same
 
-    WHITE_BOX: str = "\U00002B1C"
-    GREEN_BOX: str = "\U0001F7E9"
-    YELLOW_BOX: str = "\U0001F7E8"
-    char_counter: int = 0
-    response: str = ""
+    WHITE_BOX: str = (
+        "\U00002B1C"  # local var. making name equivalent to code for white emoji
+    )
+    GREEN_BOX: str = (
+        "\U0001F7E9"  # local var. making name equivalent to code for green emoji
+    )
+    YELLOW_BOX: str = (
+        "\U0001F7E8"  # local var. making name equivalent to code for yellow emoji
+    )
+    char_counter: int = 0  # setting index counter for each letter of word
+    response: str = ""  # preparing a str to put the response of emojis into
 
-    while char_counter < len(guess):
-        if guess[char_counter] == secret[char_counter]:
-            response += GREEN_BOX
-        elif contains_char(secret, guess[char_counter]):
-            response += YELLOW_BOX
-        else:
-            response += WHITE_BOX
-        char_counter += 1
-    return response
+    while char_counter < len(
+        guess
+    ):  # creating loop that stops after last letter of guess
+        if (
+            guess[char_counter] == secret[char_counter]
+        ):  # if letter is in word and in right index
+            response += GREEN_BOX  # add emoji to response string
+        elif contains_char(
+            secret, guess[char_counter]
+        ):  # calling contains char to check if letter is in word in wrong spot
+            response += YELLOW_BOX  # add emoji to response string
+        else:  # if letter is not in word at all
+            response += WHITE_BOX  # add emoji to response string
+        char_counter += 1  # add to index counter to move to next letter of guess
+    return response  # send string of all emojis to next part
 
 
-def main(secret: str) -> None:
+def main(secret: str) -> None:  # defining fn. to bring all fns. together
     """The entrypoint of the program and main game loop."""
-    turns: int = 0
+    turns: int = 0  # create variable for whatever turn you're playing at
 
-    while turns < 6:
-        print(f" === Turn {turns + 1}/6 ===")
+    while turns < 6:  # make loop for when turn is less than max amount of turns
+        print(
+            f" === Turn {turns + 1}/6 ==="
+        )  # print message with specific amount of turns
 
-        guess = input_guess(len(secret))
-        response = emojified(guess, secret)
-        print(response)
+        guess = input_guess(
+            len(secret)
+        )  # setting var. using input by calling input guess w len of secret
+        response = emojified(
+            guess, secret
+        )  # setting var. response as emojis given by calling emojified
+        print(response)  # printing emojis
 
-        if guess == secret:
-            print(f"You won in {turns + 1}/6 turns!")
-            exit()
+        if guess == secret:  # if you did it correctly
+            print(f"You won in {turns + 1}/6 turns!")  # print winner's statement
+            exit()  # leave program
 
-        turns += 1
+        turns += 1  # if you didn't get it, add to turns for next loop with next turn
 
-    if turns == 6:
-        print("X/6 - Sorry, try again tomorrow!")
+    if turns == 6:  # if you hit max turns
+        print("X/6 - Sorry, try again tomorrow!")  # print loser's statement
 
 
 if __name__ == "__main__":
-    main(secret="codes")
+    main(secret="codes")  # calling main with codes as secret word
